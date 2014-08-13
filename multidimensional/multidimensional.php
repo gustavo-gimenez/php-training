4d92c3f42dc2c807e092803053907dfcfbb72c1a
Gus Gimenez <gustidg@gmail.com>
2014-08-13 09:26:39 -0300

    Multi-Dimensional examples - layout

--- multidimensional/multidimensional.php
+++ multidimensional/multidimensional.php
@@ -4,3 +4,12 @@
  * Create a group of tow collection: the ten first fibonacci numbers, 
  * and ten second in another gruop like this: '1-10' => (...). "11-20" => (..)
  */
+
+/**
+ * logic here
+ */
+
+/**
+ * Print the result
+ */
+var_dump($fGroup);
