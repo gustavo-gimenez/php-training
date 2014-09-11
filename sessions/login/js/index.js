f8e3948e0fa0e3bee3488b2cd01ac8cd5e444210
gustidg <gustidg@gmail.com>
2014-09-11 18:21:29 -0300

    Using this to refer to the element the event throw in

--- sessions/login/js/index.js
+++ sessions/login/js/index.js
@@ -1,20 +1,21 @@
 $(function(){
-    algo = '';
-    $("#name").keyup(function() {
-      var inputName = $("#name");
-      var nameLength = inputName.val().length;
+    var name = $("#name");
+    name.keyup(function() {
+        var $this = $(this);
+      var nameLength = $this.val().length;
 
       if (nameLength >= 13) {
-         inputName.css('background-color', '#FFFAAA');
+         this.css('background-color', '#FFFAAA');
       } else {
-         inputName.css('background-color', '#FFFFFF');
+         this.css('background-color', '#FFFFFF');
       }
     });
     
-    $("#name").blur(function(){
-        var nameLength = $("#name").val().length;
+    name.blur(function(){
+        var $this = $(this);
+        var nameLength = $this.val().length;
         if (nameLength == 0) {     
-         $("#name").css('background-color', '#FFFAAA');
+         $this.css('background-color', '#FFFAAA');
         }
     });
 });
