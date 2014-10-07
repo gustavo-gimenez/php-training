cade90fe5cc456634049d3f08cbb8c83757448d1
gustavo <gustavo@gustavo-Lenovo-G450.(none)>
2014-10-07 20:53:47 -0300

    Adding some ajax example

---
+++ practice/cities.php
@@ -0,0 +1,5 @@
+<?php
+echo 'You selected ' . $_GET['provincia'];
+
+print json_encode(array('dada', 'dada'));
+?>
--- practice/practice.php
+++ practice/practice.php
@@ -1,6 +1,41 @@
-<?php
+<html>
+<head>
+<style>
+.container{
+  position: absolute; /* or absolute */
+  top: 50%;
+  left: 50%;
+}
+.ciudad{
+   display: none
+}
+</style>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+</head>
+<body class="container">
+<select class="provincia">
+    <option value="1">nvasyvas</option>
+    <option value="2">vgnausvns</option>
+</select>
+<select class="ciudad">
 
-/* 
- * Put here your code for each practice
- */
+</select>
+<script>
+   $(".provincia").change(function(){
+       $(".ciudad").show();
+       $(".ciudad").append('<option>dadadad</option>');
+       alert($(".provincia").val());
+
+   });
+   $.ajax({
+       url: 'cities.php',
+       data: {'provincia':$(".provincia").val() },
+       type: 'GET',
+       success: function(data){
+          $('body').append($('<p></p>').text(data));
+       }
+   });
+</script>
+</body>
+</html>
 
