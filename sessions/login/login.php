4cdbd4cddb3876838476e9222bf5ca76dc4bdb89
Facundo <isifc.facu@gmail.com>
2014-10-10 20:12:17 -0300

    examen Lucio Gabardini

--- sessions/login/login.php
+++ sessions/login/login.php
@@ -2,19 +2,21 @@
 
 session_start();
 require_once 'lib.php';
-
+//creo la cantidad de intentos o la actualiza la cantidad si ya esta creada
 if (!isset($_SESSION['cantidad'])) {
-    $_SESSION['cantidad'] = 0;
+    $_SESSION['cantidad'] = 1;
 }  else {
     $_SESSION['cantidad'] += 1;
 }
-
+//me fijo si la cantidad de intentos es menor a 3 
 if ($_SESSION['cantidad']<= 3) {
+    
+    //valida que haya completado el usuario y la pass
     if (isset($_POST['name']) && isset($_POST['password']) && !empty($_POST['name'])) {
         try {
 
             $user = getUser($_POST['name']);
-
+                //controla que el usuario y pass sean correctos
                 if (!is_null($user) && $user['password'] == md5($_POST['password'])) {
                     $_SESSION['name'] = $_POST['name'];
                     $_SESSION['pass'] = $_POST['password'];
@@ -28,7 +30,7 @@ if ($_SESSION['cantidad']<= 3) {
                 }
 
         } catch (Exception $exc) {
-            logger("Error trying to get user: ". $exc->getMessage())." van ".$_SESSION['cantidad']." errores de 3";
+            logger("Error trying to get user: ". $exc->getMessage());
 
             header('Location: error.php');
         }
@@ -37,10 +39,11 @@ if ($_SESSION['cantidad']<= 3) {
         Header("Location: index.php?error=campos vacio van ".$_SESSION['cantidad']." errores de 3");
     }
 }  else {
-                
-                Header("Location: index.php?error= You have tried to login too many times ");
-                $bloqueo = updateBlocked($_POST['name']);
+    //si el hizo mas de 3 intentos bloquea al usuario y tira el mensaje avisando que intento muchas veces loguearse
+        $bloqueo = updateBlocked($_POST['name']); 
+        Header("Location: index.php?error= You have tried to login too many times ");
+        
             
             
-        }
+}
 ?>
